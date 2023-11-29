574330b4a02ecb52c9d7aaf98556b726850ea561
ItzGalaxy15 <1073235@hr.nl>
2023-11-29 16:06:30 +0100

    test123

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pyautogui
 import os
 from models.player import Player
 
-
 def __main__() -> None:
     pygame.init()
 
@@ -11,19 +10,29 @@ def __main__() -> None:
     full_screen_size: tuple[int] = pyautogui.size()
     screen: pygame.Surface = pygame.display.set_mode(full_screen_size)
 
+    # Load background image
+    fullscreen_background = pygame.image.load(os.path.join(os.getcwd(), "image/grass.png")).convert()
+    fullscreen_background = pygame.transform.smoothscale(fullscreen_background, full_screen_size)
+    
+    original_background = pygame.image.load(os.path.join(os.getcwd(), "image/road_0.png"))
+    background_width, background_height = original_background.get_size()
+
+    # Calculate the position to center the background image
+    background_x = (full_screen_size[0] - background_width) // 2
+    background_y = (full_screen_size[1] - background_height) // 2
+    background_image = pygame.transform.smoothscale(original_background, (background_width, background_height))
     # make header
     pygame.display.set_caption("ned for spied")
     icon: pygame.Surface = pygame.image.load(
-        os.path.join(os.getcwd(), "image\siep.jpg")
-    ).convert()
+        os.path.join(os.getcwd(), "image\siep.jpg")).convert()
     pygame.display.set_icon(icon)
 
     clock = pygame.time.Clock()
 
     # player settings
     pygame.mouse.set_visible(0)
-    player_x = 250
-    player_y = 400
+    player_x = 999
+    player_y = 999
 
     # create player
     players = pygame.sprite.Group()
@@ -38,33 +47,30 @@ def __main__() -> None:
         try:
             # Process player inputs.
             for event in pygame.event.get():
-                match event.type:
-                    case pygame.QUIT:
+                if event.type == pygame.QUIT:
+                    raise SystemExit
+                elif event.type == pygame.KEYDOWN:
+                    # check which key was pressed
+                    if event.key == pygame.K_ESCAPE:
                         raise SystemExit
-                    case pygame.KEYDOWN:
-                        # check which key was pressed
-                        match event.key:
-                            case pygame.K_ESCAPE:  # esc closes screen
-                                raise SystemExit
-                            case pygame.K_w | pygame.K_UP:
-                                # go up
-                                player_dy = -player_speed
-                            case pygame.K_a | pygame.K_LEFT:
-                                # go left
-                                player_dx = -player_speed
-                            case pygame.K_s | pygame.K_DOWN:
-                                # go down
-                                player_dy = player_speed
-                            case pygame.K_d | pygame.K_RIGHT:
-                                # go right
-                                player_dx = player_speed
-                    case pygame.KEYUP:
-                        # stop moving when key is released
-                        match event.key:
-                            case pygame.K_w | pygame.K_s | pygame.K_UP | pygame.K_DOWN:
-                                player_dy = 0
-                            case pygame.K_a | pygame.K_d | pygame.K_LEFT | pygame.K_RIGHT:
-                                player_dx = 0
+                    elif event.key in (pygame.K_w, pygame.K_UP):
+                        # go up
+                        player_dy = -player_speed
+                    elif event.key in (pygame.K_a, pygame.K_LEFT):
+                        # go left
+                        player_dx = -player_speed
+                    elif event.key in (pygame.K_s, pygame.K_DOWN):
+                        # go down
+                        player_dy = player_speed
+                    elif event.key in (pygame.K_d, pygame.K_RIGHT):
+                        # go right
+                        player_dx = player_speed
+                elif event.type == pygame.KEYUP:
+                    # stop moving when key is released
+                    if event.key in (pygame.K_w, pygame.K_s, pygame.K_UP, pygame.K_DOWN):
+                        player_dy = 0
+                    elif event.key in (pygame.K_a, pygame.K_d, pygame.K_LEFT, pygame.K_RIGHT):
+                        player_dx = 0
         except SystemExit:
             pygame.quit()
             break
@@ -79,12 +85,16 @@ def __main__() -> None:
 
         # Render the graphics here.
 
+        # Draw fullscreen background image
+        screen.blit(fullscreen_background, (0, 0))
+        # Draw background image
+        screen.blit(background_image, (background_x, background_y))
+
         # draw the player(s)
         players.draw(screen)
 
         pygame.display.flip()  # Refresh on-screen display
         clock.tick(60)  # wait until the next frame (at 60 FPS)
-
 
 if __name__ == "__main__":
     __main__()
