5acb5224a0fd95b7f74de86295925a7e32bc9a4e
Perso0o0n <1075304@hr.nl>
2023-11-30 12:27:01 +0100

    player now turns correctly, added move() with collision detection in entity

--- models/entity.py
+++ models/entity.py
@@ -2,11 +2,11 @@ import pygame
 
 
 class Entity(pygame.sprite.Sprite):
-    def __init__(self, x: int, y: int, image: pygame.Surface) -> None:
+    def __init__(self, x: int, y: int, image: pygame.Surface, scale: int = 100) -> None:
         pygame.sprite.Sprite.__init__(self)
 
         # scale image
-        image_scale = 45 / image.get_rect().width
+        image_scale = scale / image.get_rect().width
         new_width = image.get_rect().width * image_scale
         new_height = image.get_rect().height * image_scale
         self.image: pygame.Surface = pygame.transform.scale(
@@ -15,3 +15,39 @@ class Entity(pygame.sprite.Sprite):
 
         self.rect: pygame.Rect = self.image.get_rect()
         self.rect.center = [x, y]
+
+    """
+moves entity based on dx and dy, and returns all entities it colides with.
+    """
+
+    def move(self, entities: pygame.sprite.Group, dx: int = 0, dy: int = 0) -> tuple:
+        # Move each axis separately. check for collision both times.
+        to_return: list = []
+        if dx != 0:
+            to_return.append(self.move_single_axis(entities.sprites(), dx, 0))
+        if dy != 0:
+            to_return.append(self.move_single_axis(entities.sprites(), 0, dy))
+        return to_return
+
+    def move_single_axis(
+        self, entities: list, dx: int = 0, dy: int = 0
+    ) -> pygame.sprite.Sprite | None:
+        # Move the rect
+        self.rect.x += dx
+        self.rect.y += dy
+
+        to_return = None
+
+        # If you collide with a wall, move out based on velocity
+        for entity in entities:
+            if self.rect.colliderect(entity.rect):
+                to_return = entity
+                if dx > 0:  # if Moving right; Hit the left side of the entity
+                    self.rect.right = entity.rect.left
+                if dx < 0:  # if Moving left; Hit the right side of the entity
+                    self.rect.left = entity.rect.right
+                if dy > 0:  # if Moving down; Hit the top side of the entity
+                    self.rect.bottom = entity.rect.top
+                if dy < 0:  # if Moving up; Hit the bottom side of the entity
+                    self.rect.top = entity.rect.bottom
+        return to_return
--- models/player.py
+++ models/player.py
@@ -1,16 +1,17 @@
-from models.entity import Entity
+from models.Entity import Entity
 import pygame
 import os
 
 
 class Player(Entity):
-    def __init__(self, x: int, y: int) -> None:
+    def __init__(self, x: int, y: int, scale: int = 100) -> None:
         image = pygame.image.load(os.path.join(os.getcwd(), "image\player.png"))
         self.default_image: pygame.Surface = image.copy()
         self.degree: int = 0
-        super().__init__(x, y, image)
+        super().__init__(x, y, image, scale)
         self.default_image: pygame.Surface = self.image.copy()
 
+    """shifts the player image in 360 degree's"""
 
     def turn(self, degree: int) -> None:
         if degree % 360 == 0 or degree == 0:
@@ -19,3 +20,9 @@ class Player(Entity):
         if degree > 0:
             degree = 360 - degree
         self.image = pygame.transform.rotate(self.default_image, degree)
+
+    """sets the player image degree to default (0)"""
+
+    def to_default(self):
+        self.image = self.default_image.copy()
+        self.degree = 0
